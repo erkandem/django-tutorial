0d3f758e530873b831b6454c046cbba97a63e06e
kan <erkan.dem@pm.me>
2020-04-12 16:53:35 +0200

    django.shortcuts.get_object_or_404 is short for a try/except clause

--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
 from django.shortcuts import render
@@ -11,10 +11,7 @@ def index(request):
     return render(request, 'my_app/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'my_app/detail.html', {'question': question})
 
 def results(request, question_id):
