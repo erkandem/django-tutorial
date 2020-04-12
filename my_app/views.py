d3e7c83158b324f35cc2d98f9a722780ce7b3868
kan <erkan.dem@pm.me>
2020-04-12 16:15:31 +0200

    first template based view accessing database

--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+from  .models import Question
 
 def index(request):
-    """simplest view in django?"""
+    """shows the first five questions by default"""
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
     return HttpResponse(
-        "Hello, world! You're at the `my_app` index page"
+        template.render(context, request)
+    )
+
+def detail(request, question_id):
+    return HttpResponse(
+        "Your're looking at question `%s`." % question_id
+    )
+def results(request, question_id):
+    return HttpResponse(
+        "Your're looking at results of question `%s`." % question_id
+    )
+def vote(requset, question_id):
+    return HttpResponse(
+        "You're voting on question `%s`." % question_id
     )
