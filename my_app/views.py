5d5aad2c782fd05a24cd212cc2a50dcab02f0396
kan <erkan.dem@pm.me>
2020-04-12 16:21:54 +0200

    bugfix didn't adjust route to my application name

--- my_app/views.py
+++ my_app/views.py
@@ -6,7 +6,7 @@ from  .models import Question
 def index(request):
     """shows the first five questions by default"""
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    template = loader.get_template('my_app/index.html')
     context = {'latest_question_list': latest_question_list}
     return HttpResponse(
         template.render(context, request)
